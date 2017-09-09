4016c7901dc3f6826ed92c5491e03f6bcdfb3bdb
18316961103 <913579394@qq.com>
2017-09-09 18:15:42 +0800

    导入第三方框架
    导入第三方框架

---
+++ BaiSiBuDeJie_Imitate/Pods/Target Support Files/Pods-BaiSiBuDeJie_Imitate/Pods-BaiSiBuDeJie_Imitate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BaiSiBuDeJie_Imitate : NSObject
+@end
+@implementation PodsDummy_Pods_BaiSiBuDeJie_Imitate
+@end
---
+++ BaiSiBuDeJie_Imitate/Pods/Target Support Files/Pods-BaiSiBuDeJie_Imitate/Pods-BaiSiBuDeJie_Imitate-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BaiSiBuDeJie_ImitateVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BaiSiBuDeJie_ImitateVersionString[];
+
